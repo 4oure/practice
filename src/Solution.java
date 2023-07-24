cc447d37dddec5cb52bf605b6e00e6e88fff657f
gavin <ultra.p0w3er@gmail.com>
2023-07-24 18:58:15 -0400

    third try

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
 	public int maxProfit(int[] prices) {
+		if(prices.length < 2)
+			return 0;
 		int highDif = 0;
-		for(int i = 0; i < prices.length; i++){
-			int newArray[] = Arrays.copyOfRange(prices, i, prices.length);
-			for (int k : newArray) {
-				if (k - newArray[0] > highDif)
-					highDif = k-newArray[0];
+		int minPrice = prices[0];
+		for(int i = 1; i < prices.length; i++) {
+			int currentProfit = prices[i] - minPrice;
+			if(currentProfit > highDif){
+				highDif=currentProfit;}
+			if(prices[i] < minPrice){
+				minPrice=prices[i];
 			}
 		}
 		return highDif;
-	}
-
-	public static void main(String[] args) {
-
 	}
 }
